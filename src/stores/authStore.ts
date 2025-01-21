dfeb8d049a1be6c62258936dab66ce9b005905c0
AnsonCar <anson1tsang@gmail.com>
2025-01-22 01:28:00 +0800

    feat: add stores

---
+++ src/stores/authStore.ts
@@ -0,0 +1,40 @@
+import { defineStore } from "pinia";
+import { refreshToken, verifyToken } from "../utils/db_api";
+
+export const useAuthStore = defineStore('authStore', {
+    state: () => ({
+        token: undefined,
+        refresh: undefined
+    } as TAuth),
+    getters: {
+
+    },
+    actions: {
+        async isLogin(): Promise<boolean> {
+            if (this.token && this.refresh) {
+                const res = await verifyToken({ token: this.token });
+                if (Object.keys(res).length === 0) {
+                    return true;
+                } else {
+                    const refreshRes = await refreshToken({ refresh: this.refresh });
+                    if (refreshRes.refresh && refreshRes.access) {
+                        this.token = refreshRes.access;
+                        this.refresh = refreshRes.refresh;
+                        return true;
+                    }
+                }
+            }
+            return false;
+        },
+        async setToken(token: string, refresh: string): Promise<void> {
+            this.token = token;
+            this.refresh = refresh;
+        },
+        async logout(): Promise<void> {
+            this.token = '';
+            this.refresh = '';
+            window.location.reload();
+        }
+    },
+    persist: true
+});
