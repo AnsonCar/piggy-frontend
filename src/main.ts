021cf7519ebbf69ad9754469eff52127f3f30023
AnsonCar <anson1tsang@gmail.com>
2025-01-22 01:28:33 +0800

    feat: update main

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { createApp } from 'vue';
 import App from './App.vue';
+import { i18n } from './i18n';
 import './index.css';
 import { router } from './routes/routes';
+import pinia from './stores';
 
 const app = createApp(App);
+
+app.use(i18n);
+app.use(pinia);
 app.use(router);
 app.mount('#app');
