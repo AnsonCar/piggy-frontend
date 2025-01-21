089b6b8b328bc73018bd29b93686c7e55a3d107e
AnsonCar <anson1tsang@gmail.com>
2025-01-22 01:27:45 +0800

    feat: add page

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,10 +1,7 @@
 <template>
-  <div>
-    <Navbar title="test" class="w-full h-64 bg-gray-900" />
-  </div>
+  test
 </template>
 
 <script setup lang="ts">
-import Navbar from '../components/Navbar.vue';
 
 </script>
