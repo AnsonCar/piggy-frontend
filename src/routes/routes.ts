dd13b55df92d2a307ff08eecfb8a44f091c72584
AnsonCar <anson1tsang@gmail.com>
2025-01-22 01:26:46 +0800

    feat: update router

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,11 +1,23 @@
-import { createMemoryHistory, createRouter } from 'vue-router';
+import { createRouter, createWebHistory } from 'vue-router';
+import BaseLayout from '../layouts/BaseLayout.vue';
+import MainLayout from '../layouts/MainLayout.vue';
 import IndexPage from '../pages/IndexPage.vue';
+import LoginPage from '../pages/LoginPage.vue';
 
 const routes = [
-    { path: '/', component: IndexPage },
+    {
+        path: '', component: MainLayout, children: [
+            { path: '', component: IndexPage, name: 'home' },
+        ]
+    },
+    {
+        path: '/login', component: BaseLayout, children: [
+            { path: '', component: LoginPage, name: 'login' },
+        ]
+    },
 ];
 
 export const router = createRouter({
-    history: createMemoryHistory(),
+    history: createWebHistory(),
     routes,
 });
