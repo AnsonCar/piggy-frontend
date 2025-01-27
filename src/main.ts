d08e2cf0d13d64a11868b153c9c349c85b141c43
AnsonCar <anson1tsang@gmail.com>
2025-01-27 22:42:10 +0800

    feat: add sober

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
 import { autoAnimatePlugin } from '@formkit/auto-animate/vue';
+import 'sober';
 import { createApp } from 'vue';
 import App from './App.vue';
 import { i18n } from './i18n';
 import './index.css';
 import router from './routes/routes';
 import pinia from './stores';
-
 const app = createApp(App);
 
 app.use(i18n);
