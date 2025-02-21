390bf62bae8de07ff20473dfb18c2740b92aa2cb
AnsonCar <anson1tsang@gmail.com>
2025-02-22 00:11:34 +0800

    feat(router): add scrollBehavior and improve code formatting
    - Add scrollBehavior function to always scroll to top when navigating
    - Improve code formatting and indentation for better readability
    - Update import statements and remove unnecessary empty lines

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -6,42 +6,47 @@ import IndexPage from '../pages/IndexPage.vue';
 import LoginPage from '../pages/LoginPage.vue';
 import TestPage from '../pages/TestPage.vue';
 
-
 const routes = [
-    {
-        path: '', component: MainLayout, children: [
-            { path: '', component: IndexPage, name: 'home' },
-        ]
-    },
-    {
-        path: '/login', component: BaseLayout, children: [
-            { path: '', component: LoginPage, name: 'login' },
-        ]
-    },
-    {
-        path: '/test', component: BaseLayout, children: [
-            { path: '', component: TestPage, name: 'test' },
-        ]
-    }
+  {
+    path: '',
+    component: MainLayout,
+    children: [{ path: '', component: IndexPage, name: 'home' }],
+  },
+  {
+    path: '/login',
+    component: BaseLayout,
+    children: [{ path: '', component: LoginPage, name: 'login' }],
+  },
+  {
+    path: '/test',
+    component: BaseLayout,
+    children: [{ path: '', component: TestPage, name: 'test' }],
+  },
 ];
 
 const router = createRouter({
-    history: createWebHistory(),
-    routes,
+  history: createWebHistory(),
+  routes,
+  scrollBehavior(to, from, savedPosition) {
+    to;
+    from;
+    savedPosition;
+    // always scroll to top
+    return { top: 0 };
+  },
 });
-
 
 // Middleware
 router.beforeEach(async (to: RouteLocationNormalizedGeneric, from: RouteLocationNormalizedLoadedGeneric) => {
-    from
-    if (to.name === 'test') return true;
-    const isLogin: boolean = await isLoggedIn();
-    if (to.name !== 'login' && !isLogin) {
-        return router.push({ name: "login" });
-    } else if (to.name === 'login' && isLogin) {
-        return router.push({ name: "home" });
-    }
-    return true;
+  from;
+  if (to.name === 'test') return true;
+  const isLogin: boolean = await isLoggedIn();
+  if (to.name !== 'login' && !isLogin) {
+    return router.push({ name: 'login' });
+  } else if (to.name === 'login' && isLogin) {
+    return router.push({ name: 'home' });
+  }
+  return true;
 });
 
 export default router;
