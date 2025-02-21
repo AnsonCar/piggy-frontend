ef5fd6ccd601da7667b69e66f2b976f4c3bdfbff
AnsonCar <anson1tsang@gmail.com>
2025-02-22 00:08:54 +0800

    feat: lang btn

--- src/components/TPLangButton.vue
+++ src/components/TPLangButton.vue
@@ -1,21 +1,47 @@
 <template>
-    <div class="dropdown dropdown-hover">
-        <TPButton tabindex="0" size="sm" role="button" icon="language" class="btn-circle btn-ghost" />
-        <ul tabindex="0" class="dropdown-content menu bg-base-100 rounded-box z-[1] w-52 p-2 shadow">
-            <li @click="changeLocale('en')"><a>English</a></li>
-            <li @click="changeLocale('zh')"><a>繁體中文</a></li>
+    <div class="dropdown dropdown-end">
+        <TPButton tabindex="0" size="sm" role="button" icon="language" class="btn-circle btn-ghost"
+            title="Change Langage Icon" name="Change Langage Icon" />
+        <ul tabindex="0"
+            class="dropdown-content menu bg-base-100 rounded-box z-[1] w-52 p-2 shadow border-2 border-[#25499A]">
+            <li @click="changeLocale('en')"><a href="#en">English</a></li>
+            <li @click="changeLocale('zh-tw')"><a href="#zh_tw">繁體中文</a></li>
+            <li @click="changeLocale('zh-cn')"><a href="#zh_cn">简体中文</a></li>
         </ul>
     </div>
 </template>
 
 <script setup lang="ts">
+import { useLanguageStore } from '@/stores/languageStore';
+import { onMounted } from 'vue';
 import { useI18n } from 'vue-i18n';
 import TPButton from './TPButton.vue';
 
 const { locale } = useI18n();
+const language = useLanguageStore();
 
-// 定义一个辅助函数来设置语言
 const changeLocale = (lang: string) => {
     locale.value = lang;
+    language.lang = lang;
+    language.user = true;
+
+    document.documentElement.lang = language.lang.toLowerCase();
 };
+
+onMounted(() => {
+    const browserLang = navigator.language.toLowerCase();
+    if (language.user) {
+        locale.value = language.lang;
+        document.documentElement.lang = language.lang.toLowerCase();
+    } else {
+        const langMap: { [key: string]: string; } = {
+            'zh-cn': 'zh-CN',
+            'zh-tw': 'zh-tw',
+            'en': 'en'
+        };
+        const defaultLang = 'zh-tw';
+        const matchedLang = Object.keys(langMap).find(key => browserLang.startsWith(key)) || defaultLang;
+        changeLocale(langMap[matchedLang]);
+    }
+});
 </script>
---
+++ src/stores/languageStore.ts
@@ -0,0 +1,11 @@
+import { defineStore } from 'pinia';
+
+export const useLanguageStore = defineStore('useLanguageStore', {
+    state: () => ({
+        lang: 'en',
+        user: false,
+    }),
+    getters: {},
+    actions: {},
+    persist: true,
+});
